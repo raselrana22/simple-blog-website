f3dc9eb812560d7832ad2bc2e9f67f1895a3badd
Rasel Rana <rasel.learn22@gmail.com>
2023-08-16 16:22:38 +0600

    add the post title number

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,14 +1,21 @@
+import Link from "next/link";
 import blogData from "../data/blogData.json"
 
 export default function Blog() {
 
     const posts = blogData;
-    console.log(posts);
 
     console.log(blogData);
     return (<div>
         {posts.map(post => {
-            return <div key={post.id}> {post.title}</div>
+            return (
+                <div key={post.id}>
+
+                    <Link href={`/post/${post.id}`}>
+                        <p>{post.id}:- {post.title}</p>
+                    </Link>
+                </div>
+            )
         })}
     </div>)
 }
