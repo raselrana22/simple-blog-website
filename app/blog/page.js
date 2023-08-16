3c85d9dabad8ab60f33c7bd1d26e6731d6ca7b66
Rasel Rana <rasel.learn22@gmail.com>
2023-08-16 13:38:41 +0600

    add blog functionality

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,3 +1,14 @@
+import blogData from "/data/blogData.json"
+
 export default function Blog() {
-    return (<div>This is the blog page</div>)
+
+    const posts = blogData;
+    console.log(posts);
+
+    console.log(blogData);
+    return (<div>
+        {posts.map(post => {
+            return <div key={post.id}> {post.title}</div>
+        })}
+    </div>)
 }
--- app/page.js
+++ app/page.js
@@ -1,3 +1,6 @@
+import Link from "next/link";
+
 export default async function Home() {
-  return (<></>);
+
+  return (<Link href={"/home"}></Link>);
 }
